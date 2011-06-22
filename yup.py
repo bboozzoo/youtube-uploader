dfaa0f4425ba5656214d2ec86e181750476b5b4e
Maciek Borzecki <maciek.borzecki@gmail.com>
2011-06-22 10:46:15 +0200

    upload works

--- yup.py
+++ yup.py
@@ -8,13 +8,20 @@ except:
     print 'python-gdata is missing'
     raise SystemExit(1)
 
+try:
+    import pycurl
+except:
+    print 'pycurl is missing'
+    raise SystemExit(1)
+
 import sys
 import os.path
+import StringIO
 from optparse import OptionParser
 from datetime import datetime
 from getpass import getpass
 
-def _upload_file(url, headers_dict, infile):
+def _upload_file(url, items_dict, infile):
     """upload a file to given url using POST method
     
     Arguments:
@@ -22,7 +29,60 @@ def _upload_file(url, headers_dict, infile):
     - `headers_dict`:
     - `infile`:
     """
-    pass
+    def progress(download_t, download_d, upload_t, upload_d):
+#        print "dt: {} dd: {} ut: {} ud: {}".format(download_t,
+#                                                   download_d,
+#                                                   upload_t,
+#                                                   upload_d)
+        if upload_t:
+            perc = 100.0 * upload_d / upload_t
+            fname = os.path.basename(infile)
+            sys.stdout.write("{} {:3.0f} % {:d}/{:d} \r".format(fname,
+                                                                perc,
+                                                                int(upload_d),
+                                                                int(upload_t)))
+            sys.stdout.flush()
+
+    c = pycurl.Curl()
+    url = url + "?nexturl=http://google.com"
+    c.setopt(pycurl.URL, url)
+    # pretend we're doing browser upload
+    items = items_dict.items()
+    items.append((os.path.basename(infile), 
+                  (pycurl.FORM_FILE, infile)))
+    c.setopt(pycurl.HTTPPOST, items)
+
+    # progress
+    c.setopt(pycurl.NOPROGRESS, 0)
+    c.setopt(pycurl.PROGRESSFUNCTION, progress)
+
+    # need to check header if redirection to nexturl was ok
+    header = StringIO.StringIO()
+    c.setopt(pycurl.HEADERFUNCTION, header.write)
+    # upload
+    c.perform()
+    # newline after showing progress
+    print "\n"
+
+    hlines = header.getvalue().splitlines()
+    # look for location
+    hentries = dict([line.split(":", 1) for line in hlines if ":" in line])
+    if not "Location" in hentries:
+        raise YoutubeUploaderError("Location not found in header??")
+    loc = hentries["Location"]
+    loc_param = loc.split("?", 1)[1]
+    get_dict = dict([entry.split("=") for entry in loc_param.split("&") if "=" in entry])
+    if not "status" in get_dict:
+        raise YoutubeUploaderError("status missing from Location: " 
+                                   + hentries["Location"])
+    status = int(get_dict["status"])
+
+    # HTTP return needs to be 302 and status from Location needs to be 200
+    if c.getinfo(pycurl.HTTP_CODE) != 302 or \
+            status != 200:
+        return False
+
+    return True
 
 class YoutubeUploaderError(Exception):
     """error
@@ -83,7 +143,7 @@ class YoutubeUploader(object):
         retry = True
         while retry:
             try:
-                print "Login with user %s" % (self._user)
+                print "--- Login with user %s" % (self._user)
                 self._yt.ProgrammaticLogin(captcha_token = captcha_token,
                                            captcha_response = captcha_response)
                 retry = False
@@ -107,7 +167,7 @@ class YoutubeUploader(object):
         Arguments:
         - `filename`: video file
         """
-        print "uploading %s" % (filename)
+        print "--- Uploading %s" % (filename)
 
         name = os.path.basename(os.path.expanduser(filename))
         gd_media = gdata.media.Group(
@@ -121,10 +181,13 @@ class YoutubeUploader(object):
         gd_video = gdata.youtube.YouTubeVideoEntry(media = gd_media)
         
         url, token = self._yt.GetFormUploadToken(gd_video)
-        print url, token
+        #print url, token
 
         # upload file
-        _upload_file(url, headers, filename)
+        if _upload_file(url, {"token" : token } , filename):
+            print "   done"
+        else:
+            print "   failed"
         
 if __name__ == '__main__':
     usage = "usage: %prog -u <user> -f <file> [-f <file>]"
